e6f14183aaa9b741e1981f1483f374aa5b2b4c7a
Tamar Tavor <tamartavor22.gmail.com>
2024-04-17 15:20:10 +0300

    mysql port

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const connection = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 // Connect to MySQL
